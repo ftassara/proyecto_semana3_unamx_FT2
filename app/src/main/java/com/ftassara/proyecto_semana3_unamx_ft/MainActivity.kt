60e81b2bae80f0d8f3ef15dabb291a37ebf43b2b
ftassara <francisco.tassara@gmail.com>
2020-09-22 00:52:53 -0300

    1ra version

---
+++ app/src/main/java/com/ftassara/proyecto_semana3_unamx_ft/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ftassara.proyecto_semana3_unamx_ft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
